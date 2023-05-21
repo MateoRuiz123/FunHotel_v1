6b58f170a3a77715e8b2f7c30e43f88f34e73bed
MateoRuiz123 <mrivera736@misena.edu.co>
2023-05-21 11:59:18 -0500

    'Primer Commit'

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth']); // Add middleware to protect route
+ 
