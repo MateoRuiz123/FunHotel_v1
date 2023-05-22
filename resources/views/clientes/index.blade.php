116b0c222a4e308d27b0f012cecd6b15ee305586
MateoRuiz123 <mrivera736@misena.edu.co>
2023-05-22 00:47:16 -0500

    faltan los modales o ventanas del crud

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,271 +1,79 @@
 <!-- Vista principal -->
-@extends('home')
+@extends('layouts.app')
 <!-- extendido del home -->
 @section('content')
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {{-- <link rel="stylesheet" href="resources/css/app.css"/> --}}
-        <link rel="stylesheet" href="{{ asset('css/clientes.css') }}">
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
-
-        <title>Clientes</title>
-    </head>
-
-    <body>
-
-        <!-- Begin page -->
-        <div id="layout-wrapper">
-
-
-            <header id="page-topbar">
-                <div class="navbar-header">
-                    <div class="d-flex">
-                        <!-- LOGO -->
-                        <div class="navbar-brand-box">
-                            <a href="index.html" class="logo logo-dark">
-                                <span class="logo-sm">
-                                    <img src="assets/images/logo-sm.png" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/logo-dark.png" alt="" height="17">
-                                </span>
-                            </a>
-
-                            <a href="index.html" class="logo logo-light">
-                                <span class="logo-sm">
-                                    <img src="assets/images/logo-sm.png" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/logo-light.png" alt="" height="18">
-                                </span>
-                            </a>
-                        </div>
-
-                        <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect"
-                            id="vertical-menu-btn">
-                            <i class="mdi mdi-menu"></i>
-                        </button>
-                    </div>
-
-                    <div class="d-flex">
-
-                        <div class="dropdown d-none d-lg-inline-block">
-                            <button type="button" class="btn header-item noti-icon waves-effect"
-                                data-bs-toggle="fullscreen">
-                                <i class="mdi mdi-fullscreen"></i>
-                            </button>
-                        </div>
-
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img class="rounded-circle header-profile-user" src="assets/images/users/user-4.jpg"
-                                    alt="Header Avatar">
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <a class="dropdown-item" href="#"><i
-                                        class="mdi mdi-account-circle font-size-17 align-middle me-1"></i> Profile</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item text-danger" href="#"><i
-                                        class="bx bx-power-off font-size-17 align-middle me-1 text-danger"></i> Logout</a>
-                            </div>
-                        </div>
-
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect">
-                                <i class="mdi mdi-cog-outline"></i>
-                            </button>
-                        </div>
-
-                    </div>
-                </div>
-            </header>
-
-            <!-- ========== Left Sidebar Start ========== -->
-            <div class="vertical-menu">
-
-                <div data-simplebar class="h-100">
-
-                    <!--- Sidemenu -->
-                    <div id="sidebar-menu">
-                        <!-- Left Menu Start -->
-                        <ul class="metismenu list-unstyled" id="side-menu">
-                            <li class="menu-title">Main</li>
-
-                            <li>
-                                <a href="index.html" class="waves-effect">
-                                    <i class="ti-home"></i><span class="badge rounded-pill bg-primary float-end">1</span>
-                                    <span>Dashboard</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="calendar.html" class=" waves-effect">
-                                    <i class="ti-calendar"></i>
-                                    <span>Calendar</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="javascript: void(0);" class="has-arrow waves-effect">
-                                    <i class="ti-email"></i>
-                                    <span>Email</span>
-                                </a>
-                                <ul class="sub-menu" aria-expanded="false">
-                                    <li><a href="email-inbox.html">Inbox</a></li>
-                                    <li><a href="email-read.html">Email Read</a></li>
-                                    <li><a href="email-compose.html">Email Compose</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- Sidebar -->
-                </div>
+    <!-- start page title -->
+    <div class="page-title-box">
+        <div class="row align-items-center">
+            <div class="col-md-8">
+                <h6 class="page-title">Datos clientes</h6>
             </div>
-            <!-- Left Sidebar End -->
 
+            <div class="col-md-4">
+                <div class="float-end d-none d-md-block">
 
-
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
-
-                <div class="page-content">
-                    <div class="container-fluid">
-
-                        <!-- start page title -->
-                        <div class="page-title-box">
-                            <div class="row align-items-center">
-                                <div class="col-md-8">
-                                    <h6 class="page-title">Datos clientes</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- end page title -->
-
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-body">
-
-                                        <h4 class="card-title">Botones</h4>
-
-                                        <table id="datatable-buttons"
-                                            class="table table-striped table-bordered dt-responsive nowrap"
-                                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">Nombre</th>
-                                                    <th scope="col">Segundo Nombre</th>
-                                                    <th scope="col">Apellido</th>
-                                                    <th scope="col">Segundo Apellido</th>
-                                                    <th scope="col">Tipo documento</th>
-                                                    <th scope="col">Documento</th>
-                                                    <th scope="col">Celular</th>
-                                                    <th scope="col">Correo</th>
-                                                    <th scope="col">Acciones</th>
-                                                </tr>
-                                            </thead>
-
-
-                                            <tbody>
-                                                @foreach ($clientes as $cliente)
-                                                    <tr>
-                                                        <td>{{ $cliente->primerNombre }}</td>
-                                                        <td>{{ $cliente->segundoNombre }}</td>
-                                                        <td>{{ $cliente->primerApellido }}</td>
-                                                        <td>{{ $cliente->segundoApellido }}</td>
-                                                        <td>{{ $cliente->tipoDocumento }}</td>
-                                                        <td>{{ $cliente->numeroDocumento }}</td>
-                                                        <td>{{ $cliente->celular }}</td>
-                                                        <td>{{ $cliente->correo }}</td>
-                                                        <td>
-                                                            <button type="button" class="btn btn-warning"
-                                                                data-toggle="modal"
-                                                                data-target="#EDITAR{{ $cliente->id }}">
-                                                                <i class="bi bi-pencil-square" id="icons"></i>
-                                                            </button>
-                                                            <button type="button" class="btn btn-danger"
-                                                                data-toggle="modal"
-                                                                data-target="#ELIMINAR{{ $cliente->id }}">
-                                                                <i class="bi bi-trash3"></i>
-                                                            </button>
-                                                        </td>
-                                                    </tr>
-                                                    @include('clientes.delete')
-                                                    @include('clientes.update')
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-
-                                    </div>
-                                </div>
-                            </div> <!-- end col -->
-                        </div> <!-- end row -->
-
-                        @include('clientes.create')
-
-                    </div> <!-- container-fluid -->
-                </div>
-                <!-- End Page-content -->
-
-
-            </div>
-            <!-- end main content-->
-
-        </div>
-        <!-- END layout-wrapper -->
-
-        <!-- Right Sidebar -->
-        <div class="right-bar">
-            <div data-simplebar class="h-100">
-                <div class="rightbar-title px-3 py-4">
-                    <a href="javascript:void(0);" class="right-bar-toggle float-end">
-                        <i class="mdi mdi-close noti-icon"></i>
+                    <a href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#crear">
+                        Registrar <i class="bi bi-person-plus" id="icons"></i>
                     </a>
-                    <h5 class="m-0">Settings</h5>
+
                 </div>
-
-                <!-- Settings -->
-                <hr class="mt-0" />
-                <h6 class="text-center">Choose Layouts</h6>
-
-                <div class="p-4">
-                    <div class="mb-2">
-                        <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="">
-                    </div>
-                    <div class="form-check form-switch mb-3">
-                        <input type="checkbox" class="form-check-input theme-choice" id="light-mode-switch" checked />
-                        <label class="form-check-label" for="light-mode-switch">Light Mode</label>
-                    </div>
-
-                    <div class="mb-2">
-                        <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="">
-                    </div>
-                    <div class="form-check form-switch mb-3">
-                        <input type="checkbox" class="form-check-input theme-choice" id="dark-mode-switch"
-                            data-bsStyle="assets/css/bootstrap-dark.min.css"
-                            data-appStyle="assets/css/app-dark.min.css" />
-                        <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
-                    </div>
-                </div>
-
-            </div> <!-- end slimscroll-menu-->
+            </div>
         </div>
-        <!-- /Right-bar -->
+    </div>
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
-    @endsection
-    <script src="{{ asset('js/validacion.js') }}"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" />
-</body>
+                <h4 class="card-title">Botones</h4>
 
-</html>
+                <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+                    style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Segundo Nombre</th>
+                            <th>Apellido</th>
+                            <th>Segundo Apellido</th>
+                            <th>Tipo documento</th>
+                            <th>Documento</th>
+                            <th>Celular</th>
+                            <th>Correo</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+                        @foreach ($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->primerNombre }}</td>
+                                <td>{{ $cliente->segundoNombre }}</td>
+                                <td>{{ $cliente->primerApellido }}</td>
+                                <td>{{ $cliente->segundoApellido }}</td>
+                                <td>{{ $cliente->tipoDocumento }}</td>
+                                <td>{{ $cliente->numeroDocumento }}</td>
+                                <td>{{ $cliente->celular }}</td>
+                                <td>{{ $cliente->correo }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-warning" data-toggle="modal"
+                                        data-target="#EDITAR{{ $cliente->id }}">
+                                        <i class="bi bi-pencil-square" id="icons"></i>
+                                    </button>
+                                    <button type="button" class="btn btn-danger" data-toggle="modal"
+                                        data-target="#ELIMINAR{{ $cliente->id }}">
+                                        <i class="bi bi-trash3"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            @include('clientes.delete')
+                            @include('clientes.update')
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div> <!-- end col -->
+
+    @include('clientes.create')
+@endsection
